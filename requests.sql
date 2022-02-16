129416c1dbdb7aeb117b13aee7e5a3863fc7b0f5
arty4170 <kibitovartem88@gmail.com>
2022-02-16 08:06:47 +0200

    save in dir

--- requests.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `authors` (`name`) VALUES ('john'), ('jane'), ('mark'), ('dave'), ('frank');
-DELETE FROM `authors` WHERE `name`='john';
-DELETE FROM `authors`;
-SELECT * FROM `authors` WHERE `id`<3;
-SELECT * FROM `authors`;
-UPDATE `authors` SET `name`='john smith' WHERE `name`='john'; 
-SELECT * FROM `authors` WHERE `name`='john smith'; 
